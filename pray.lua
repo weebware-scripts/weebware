d8f7f39624cca0c2ab70d7184e1a79d60cad7556
weebware-scripts <108202532+weebware-scripts@users.noreply.github.com>
2022-06-26 03:48:19 +0200

    Delete pray.lua

--- pray.lua
+++
@@ -1,7 +0,0 @@
-for i,v in pairs(game:GetService('Players'):GetChildren()) do
-   AnimationId = "3487719500"
-   local Anim = Instance.new("Animation")
-   Anim.AnimationId = "rbxassetid://"..AnimationId
-   local k = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
-   k:Play() --Play the animation
-end
